7e6e97ddf0a26a90f4dd87965702c526f4b17cea
vitor <vitorduarte.vds7@gmail.com>
2022-07-06 16:01:53 -0300

    feat: cpf validation

--- src/index.js
+++ src/index.js
@@ -9,12 +9,20 @@ const customers = [];
 
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists" });
+    }
+
     const id = uuidv4();
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: [],
     });
 
